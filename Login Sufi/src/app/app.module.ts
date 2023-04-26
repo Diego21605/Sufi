5d923bc19cdb2182ab66be288dc7ede212c2ccd2
Diego Fernandez <54678330+Diego21605@users.noreply.github.com>
2023-04-25 22:19:42 -0500

    Update

---
+++ Login Sufi/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Vistas/login/login.component';
+import { CardModule } from 'primeng/card';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
